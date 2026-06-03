32966ae74126b0ccf47439af9b324648ac420975
Rinat Tabaev <rdtabaev@edu.hse.ru>
2026-06-03 22:54:59 +0300

    issue setup

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -10,22 +10,14 @@ const __dirname = path.dirname(__filename);
 const rootDir = process.cwd();
 const srcDir = path.join(rootDir, "./src");
 
-async function runBuildWatcher() {
-    await build({ watch: {} });
+async function runBuild() {
+    await build();
 
     console.log("\n waiting for file changes");
 };
 
-runBuildWatcher();
+runBuild();
 
-// async function runBuild() {
-//     await build();
-
-//     console.log("\n waiting for file changes");
-// };
-
-// runBuild();
-
-// fs.watch(srcDir, { recursive: true }, () => {
-//     runBuild();
-// });
+fs.watch(srcDir, { recursive: true }, () => {
+    runBuild();
+});
